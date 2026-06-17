863bc3f2882bf02ea9e0a4266472505b33b14046
jasersans1-create <jasersans1@gmail.com>
2026-06-17 13:31:55 +0530

    initial HCB subpage work

---
+++ HackHive/src/pages/HCB.tsx
@@ -0,0 +1,14 @@
+export defauly function HCB() {
+return(
+
+<div className="min-h-screen bg-black text-white">
+<section className="h-screen flex items-center justify-center">
+<div>
+<h1 className="text-8xl font-black">BUILD.</h1>
+<p>
+                    How Hack Club helps students build.
+</p>
+
+
+</div>
+</section>
